878ae3397ba9c6f85f3a61418bc0554e378f59a6
bruce.li <bruce.li@airwallex.com>
2023-03-25 17:11:47 +0800

    respond to ping

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                // Wait for client to send us a message, but ignore the content for now
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+                print!("{:?}", buf);
+                stream.write("+PONG\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
