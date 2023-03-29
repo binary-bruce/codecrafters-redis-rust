9b5f50f8cbe0fe5ba646eacffc654158921f4f4b
bruce.li <bruce.li@airwallex.com>
2023-03-29 22:13:58 +0800

    echo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
-use bytes::BytesMut;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use resp::Value::{Error, SimpleString};
 use tokio::net::{TcpListener, TcpStream};
+
+mod resp;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -22,17 +23,22 @@ async fn main() -> Result<()> {
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    let mut buf = BytesMut::with_capacity(512);
+async fn handle_connection(stream: TcpStream) -> Result<()> {
+    let mut conn = resp::RespConnection::new(stream);
     loop {
-        // Wait for the client to send us a message but ignore the content for now
-        let bytes_read = stream.read_buf(&mut buf).await?;
-        if bytes_read == 0 {
-            println!("client closed the connection!");
+        let value = conn.read_value().await?;
+        if let Some(value) = value {
+            let (command, args) = value.to_command()?;
+            let response = match command.to_ascii_lowercase().as_ref() {
+                "ping" => SimpleString("PONG".to_string()),
+                "echo" => args.first().unwrap().clone(),
+                _ => Error(format!("command not implemented: {}", command)),
+            };
+
+            conn.write_value(response).await?;
+        } else {
             break;
         }
-
-        stream.write("+PONG\r\n".as_bytes()).await?;
     }
 
     Ok(())
